5f82ecbe18b3b1a4030b6a82ed868fca873b2d95
Vishal Sharma <vishalsharma18250@gmail.com>
2026-02-21 13:03:17 +0530

    Learned about array in js

---
+++ Javascript-Classwork/js-basics/07-array.js
@@ -0,0 +1,24 @@
+const passanger = ["Veer","Kushal","Umang"];
+const empetyCarriage = [];
+
+console.log(empetyCarriage.length);
+
+const threeEmpetySeats = Array(3);
+console.log(threeEmpetySeats.length);
+
+const singlePassanger = Array.of(3);
+console.log(singlePassanger);
+
+const trainCode = Array.from("DUST");
+console.log(trainCode);
+
+const tempTrain = ["A","B","C","D","E"]
+tempTrain.length = 3;
+console.log(tempTrain)
+tempTrain.length = 5;
+console.log(tempTrain);
+
+
+
+
+
